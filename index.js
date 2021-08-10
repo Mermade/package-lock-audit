e6e7cb07ccefa29b2004917bf8af39125a86f664
Mike Ralphson <mike.ralphson@gmail.com>
2021-08-10 16:53:39 +0100

    fix: most basic support for lockfileVersion 2
    Signed-off-by: Mike Ralphson <mike.ralphson@gmail.com>

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ const lblock = ['GPL','AGPL','GPL-3.0','GPL-2.0','AGPL-2.0','AGPL-3.0'];
 let dep = {};
 
 function audit(obj,argv) {
-  assert.equal(obj.lockfileVersion,1,'Expected lockfileVersion 1');
+  assert.ok(obj.lockfileVersion <= 2,'Expected lockfileVersion 1 or 2');
+  delete obj.packages; // lockfileversion 2 packages object contains objects called dependencies with a different structure
   console.log('Checking',obj.name,obj.version);
   recurse(obj,{},function(obj,key,state){
     if (key === 'dependencies' && typeof obj[key] === 'object') {
